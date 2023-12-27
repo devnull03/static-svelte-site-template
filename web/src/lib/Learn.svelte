269f12d02fa6311b991dee7dcc7c995f3d2fd5ad
devnull03 <56480041+devnull03@users.noreply.github.com>
2023-12-27 00:59:31 -0800

    fix: learn page css fix

--- web/src/lib/Learn.svelte
+++ web/src/lib/Learn.svelte
@@ -11,14 +11,13 @@
 
     onMount(() => {
         questions = _.shuffle(questions);
-    })
-
+    });
 </script>
 
 <div class="flex flex-col justify-evenly items-center mt-10">
-    <div class="flex flex-col w-8/12 gap-10">
+    <div class="flex flex-col w-8/12 gap-14">
         {#each questions as question, i}
-            <div class="flex flex-col justify-between">
+            <div class="flex flex-col justify-between gap-4">
                 <div class="flex flex-row font-semibold mb-5">
                     <div
                         class="flex justify-center bg-gray-800 w-7 h-7 text-white me-2"
@@ -27,19 +26,21 @@
                     </div>
                     <div class="">{question.question}</div>
                 </div>
-                <div>
-                    {#if question.question_image}
+                {#if question.question_image}
+                    <div>
                         <img
                             src={question.question_image}
                             alt=""
                             class="h-32 object-contain"
                         />
-                    {/if}
-                </div>
+                    </div>
+                {/if}
 
-                <div class="flex flex-row h-16">
-                    <div class="bg-green-500 h-full p-5">Answer:</div>
-                    <div class=" bg-green-100 w-full ps-2 h-full pt-5">
+                <div
+                    class="flex flex-row text-left rounded-lg border hover:shadow-inner transition-all duration-200"
+                >
+                    <div class="bg-green-500 p-5 rounded-l-lg">Answer:</div>
+                    <div class=" bg-green-100 p-5 w-full rounded-r-lg">
                         {question.answer}
                     </div>
                 </div>
