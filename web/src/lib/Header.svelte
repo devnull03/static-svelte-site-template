707d0327f8085439ca9062b175dc9ac3df0a9cf3
devnull03 <56480041+devnull03@users.noreply.github.com>
2023-12-29 02:32:20 -0800

    feat: google translate

--- web/src/lib/Header.svelte
+++ web/src/lib/Header.svelte
@@ -1,42 +1,41 @@
 <script lang="ts">
-  const navLinks = [
-    {
-      name: "Home",
-      href: "/",
-    },
-    // {
-    //   name: "My test",
-    //   href: "/about",
-    // },
-    // {
-    //   name: "Logout",
-    //   href: "/contact",
-    // },
-    
-  ];
+    import { onMount } from "svelte";
+    import { GoogleTranslate } from "@candidosales/svelte-google-translate";
 
+    const navLinks = [
+        {
+            name: "Home",
+            href: "/",
+        },
+    ];
 </script>
 
+<GoogleTranslate
+    elementId={"google-translate-element"}
+    options={{ pageLanguage: "en", includedLanguages: "pt,en,pa,hi,zh,tl,ja" }}
+/>
+
 <nav class="">
-  <div
-    class="top-0 bg-sky-500 h-16 w-full flex flex-row justify-between items-center fixed z-50 "
-  >
-    <!-- top fixed -->
-    <div class="flex text-white font-bold items-center ps-4 "> the driving school</div>
+    <div
+        class="top-0 bg-sky-500 h-20 w-full flex flex-row justify-between items-center fixed z-50"
+    >
+        <!-- top fixed -->
+        <div class="flex text-white font-bold items-center ps-4">
+            the driving school
+        </div>
 
-    <div class="flex flex-row justify-evenly gap-8 items-center pe-4">
-      <!-- nav links -->
-      
-      {#each navLinks as link}
-        <a
-          href={link.href}
-          class="text-xs uppercase text-white hover:text-opacity-80 transition-colors duration-300 ease-in-out"
-        >
-          {link.name}
-        </a>
-      {/each}
+        <div class="flex flex-row justify-evenly gap-8 items-center pe-4">
+            <!-- nav links -->
+
+            {#each navLinks as link}
+                <a
+                    href={link.href}
+                    class="text-xs uppercase text-white hover:text-opacity-80 transition-colors duration-300 ease-in-out"
+                >
+                    {link.name}
+                </a>
+            {/each}
+            <div id="google-translate-element"></div>
+        </div>
     </div>
-
-    
-  </div>
 </nav>
