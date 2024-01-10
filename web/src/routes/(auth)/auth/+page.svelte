0bdf14fd00edd99aa914bc2f15a6377d8e7fa1e6
devnull03 <56480041+devnull03@users.noreply.github.com>
2024-01-09 23:08:38 -0800

    feat: otp login

--- web/src/routes/(auth)/auth/+page.svelte
+++ web/src/routes/(auth)/auth/+page.svelte
@@ -1,35 +1,36 @@
 <!-- // src/routes/auth/+page.svelte -->
 <script lang="ts">
-    import { fly, slide } from 'svelte/transition';
+    import { goto } from "$app/navigation";
+    import { fly, slide } from "svelte/transition";
 
     export let data;
-    let { supabase } = data;
-    $: ({ supabase } = data);
+    let { supabase, session } = data;
+    $: ({ supabase, session } = data);
 
     let email: string;
-    let password: string;
+    let otp: string;
+    let errorMessage: string = "";
 
-    let showMessage = false;
-
-    const handleSignUp = async (event: SubmitEvent) => {
-        event.preventDefault();
-        let res = await supabase.auth.signUp({
-            email,
-            password,
-            options: {
-                emailRedirectTo: `${location.origin}/auth/callback`,
-            },
-        });
-        console.log(res);
-    };
+    let otpSent = false;
 
     const handleSignIn = async () => {
-        let res = await supabase.auth.signInWithPassword({
+        console.log(email, otp);
+
+        let res = await supabase.auth.verifyOtp({
             email,
-            password,
+            token: otp,
+            type: "email",
         });
 
         console.log(res);
+
+        if (res.error) {
+            errorMessage = res.error.message;
+        } else {
+            // console.log(res);
+            // session = res;
+            goto("/");
+        }
     };
 
     const handleSignOut = async () => {
@@ -37,11 +38,7 @@
         console.log(res);
     };
 
-    async function signInWithEmail(event: SubmitEvent) {
-        event.preventDefault();
-
-        showMessage = true;
-
+    async function signInWithEmail() {
         let res = await supabase.auth.signInWithOtp({
             email,
             options: {
@@ -52,17 +49,24 @@
         });
 
         console.log(res);
+
+        if (res.error) {
+            errorMessage = res.error.message;
+        } else {
+            otpSent = true;
+        }
     }
 </script>
 
 <div class="mt-20 flex flex-col gap-8 items-center px-4">
-    <form
-        on:submit={signInWithEmail}
-        class="bg-gray-100 flex flex-col gap-6 w-full lg:w-1/2 h-[50vh] items-center justify-center rounded-lg relative"
+    <div
+        class="bg-gray-100 flex flex-col gap-2 w-full lg:w-1/2 h-[50vh] items-center justify-center rounded-lg relative"
     >
         <div class="top-8 absolute text-center">
             <h1 class="text-4xl font-bold pb-4">Sign In</h1>
-            <p class="text-sm">Enter your email and click the link in your inbox to sign in.</p>
+            <p class="text-sm">
+                Enter your email and click the link in your inbox to sign in.
+            </p>
         </div>
 
         <span class="flex flex-row gap-4 items-center pt-8">
@@ -70,6 +74,7 @@
             <input
                 class="outline-none rounded-md px-2 py-1 shadow"
                 placeholder="example@domain.com"
+                disabled={otpSent}
                 id="email"
                 type="email"
                 name="email"
@@ -77,16 +82,44 @@
             />
         </span>
 
-        {#if showMessage}
-            <div transition:slide={{  }}>
-                <p class="text-sm">Check your email for the magic link!</p>
+        {#if otpSent}
+            <span transition:slide class="flex flex-row gap-4 items-center">
+                <span class="text-lg"> OTP: </span>
+                <input
+                    class="outline-none rounded-md px-2 py-1 shadow"
+                    placeholder="123456"
+                    id="otp"
+                    type="text"
+                    name="otp"
+                    bind:value={otp}
+                />
+            </span>
+        {/if}
+
+        {#if errorMessage}
+            <div transition:slide>
+                <p class="text-sm text-red-500">{errorMessage}</p>
             </div>
         {/if}
 
         <button
+            on:click={(e) => {
+                e.preventDefault();
+
+                if (otpSent) {
+                    handleSignIn();
+                } else {
+                    signInWithEmail();
+                }
+            }}
             class="border py-1 px-2 rounded-md shadow font-light hover:scale-105 active:scale-100 ease-linear duration-200 disabled:opacity-50 disabled:cursor-not-allowed"
-            disabled={!email || showMessage}
-            >Sign in</button
+            disabled={!email || (otpSent && !otp)}
         >
-    </form>
+            {#if otpSent}
+                Sign In
+            {:else}
+                Send OTP
+            {/if}
+        </button>
+    </div>
 </div>
